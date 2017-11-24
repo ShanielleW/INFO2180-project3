30caa58a58f7dcc2015925c1a2b400bd12a35e95
mossponsol <mossenialponsol@gmail.com>
2017-11-24 18:01:27 -0500

    temp, error abound.

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$("#logBtn").on('click', function(event){
+		event.preventDefault(); //don't change, button submits immediately otherwise
+
+		var username = $("#usname").val();
+		var password = $("#pass").val();
+
+		var urlInfo = "username="+ username + "&password=" + password;
+		var XMLhttp = new XMLHttpRequest();
+
+		XMLhttp.onreadystatechange = function(){
+			if (this.readystatechange == 4)
+				if(this.status==200){
+					//get homepage for user
+					var info = "cheapomail.php?mail=true";
+					$.ajax(link,{
+						method: 'GET'
+					}).done(function(mails){
+						$("#main").html(mails); //changes the main area to load new html 
+					})
+				}
+		}
+	});
+	XMLhttp.open("POST", "cheapomail.php", true);
+    XMLhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    XMLhttp.send(dat);
+});
