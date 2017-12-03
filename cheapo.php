b3102f7908b7920bee219e891248dda6865ec6b3
randycaine1 <nicoycaine1@gmail.com>
2017-12-03 15:57:35 -0500

    Send and Receive

--- cheapo.php
+++ cheapo.php
@@ -64,6 +64,70 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $sql = "INSERT INTO message_read(read_id,message_id) VALUES('$readID','')"
     $connect->exec($sql);
   }
+  //send a message
+    if (isset($recps) && isset($subj) && isset($body)){
 
+        //get id of sender
+        $sid = $_SESSION["user_id"];
+
+        $cdate = date("Y/m/d");
+
+        $recps = explode(",", $recps);
+
+        foreach($recps as $recp){
+
+            //get id of receiver
+            $stmt2 = $conn->query("SELECT id FROM users WHERE username = '$recp'");
+            $s = $stmt2->fetch();
+            $rid = $s["id"];
+
+            // query to be sent
+            $q = "INSERT INTO message(recipients_id, user_id, subject, body, sentdate) VALUES('$rid', '$sid', '$subj', '$body', '$cdate');";
+            $conn->exec($q);
+        }
+
+        echo 'Message Sent';
+
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            // user id to recieve mail
+            $rcvr = $_SESSION["user_id"];
+            $getmail = strip_tags($_GET["getmail"]);
+
+            if ($getmail == 'true'){
+
+                $stmt = $conn->query("SELECT * FROM message WHERE recipients_id = '$rcvr' ORDER BY sentdate LIMIT 10;");
+                $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                $stmt2 = $conn->query("SELECT message_id FROM message_read;");
+                $res2 = $stmt2->fetchAll(PDO::FETCH_COLUMN, 0);
+
+                if(count($res) == 0){
+                    echo "<h2>No Mail Found</h2>";
+                }
+
+                else{
+                    foreach($res as $mail){
+
+                        $new = $conn->query("SELECT username FROM user WHERE id = '" . $mail["user_id"] . "';");
+                        $sendr = $new->fetch();
+
+                        if (in_array($mail["id"], $res2)){
+                            echo '<div class="mail read">';
+                            echo '<p>(Read)</p>';
+                        }
+                        else{
+                            echo '<div class="mail unread">';
+                        }
+
+                        echo '<p>From: ' . $sendr["username"] . '</p>';
+                        echo '<p>Subject: ' . $mail["subject"] . '</p>';
+                        echo '<p class="recv">Message: ' . $mail["body"] . '</p>';
+                        echo '<input type="submit" class="showbutton" value="Read"/>';
+                        echo '<p class="hide">' . $mail["id"] . '</p>';
+                        echo '</div> <br><br>';
+                    }
+                }
+              }
+            }
 
 ?>
